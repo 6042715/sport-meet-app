20a84f6cf584f8534d37d0cfd1d88bc2954f09e9
Felix Nieuwenhuijsen <6042715@mborijnland.nl>
2025-04-14 14:32:57 +0200

    bericht modus toegevoegd

---
+++ chat.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style/main-style.css">
+</head>
+<body>
+    <header></header>
+    <main id="chatMain">
+        <section id="chatSection">
+            <!-- voorbeeld chat (wordt meteen verwijderd) -->
+            <div class="chatBubble" id="templateMSG">
+                <img src="img/placeholders/profile1.png">
+                <div class="chatContent">
+                    <h3 class="chatName">Jan koekepan</h3>
+                    <p class="chatText">ohhh ja, ik hou zo van sporten, hell yeah!!! boom
+                        boom boom boom boom!!!!!
+                    </p>
+                </div>
+            </div>
+        </section>
+        <form id="chatInput">
+            <textarea id="textInput" rows="1"></textarea>
+            <input type="submit" id="sendButton">
+        </form>
+    </main>
+    <?php
+    include_once("templates/footer.php");
+    ?> 
+    <script src="lib/chatTest.js" defer></script>
+</body>
+</html>

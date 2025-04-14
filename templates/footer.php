f54bf5e54c6d5724b7554daccabd2c33dd06cfd8
Felix Nieuwenhuijsen <6042715@mborijnland.nl>
2025-04-14 11:12:20 +0200

    footer gemaakt

---
+++ templates/footer.php
@@ -0,0 +1,20 @@
+<footer>
+    <nav>
+        <a id="home">
+            <p>🏠</p>
+            <p>home</p>
+        </a>
+        <a id="calendar">
+            <p>📅</p>
+            <p>events</p>
+        </a>
+        <a id="chat">
+            <p>💬</p>
+            <p>chat</p>
+        </a>
+        <a id="profile">
+            <p>👤</p>
+            <p>profile</p>
+        </a>
+    </nav>
+</footer>
